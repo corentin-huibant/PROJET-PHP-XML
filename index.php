fda73b007bbc505ad98bca421e3abf4db27d0e87
quentinpimont <quentin.pimont.60@gmail.com>
2017-05-26 15:05:48 +0200

    reécriture des liens

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <?php
+// instancie un objet DOMDocument
 $dom = new DOMDocument();
-$dom->validateOnParse = true;
+// charge le fichier xml voulu
 $dom->load('source.xml');
+// variable representant le numero de la page courante
 $page = 0;
-foreach ($dom->getElementsByTagName('menu') as $position => $btn) {
-    if (isset($_POST[$position])) {
-        $page = $position;
+// permet changer la valeur de la variable $page selon le bouton appuyer
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'] - 1;
     }
-}
+
 ?>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8"/>
         <title><?= $dom->getElementsByTagName('title')->item($page)->nodeValue ?></title>
     </head>
     <body>
         <form method="POST" action="index.php">
             <?php
+            // créer les boutons de naviguation 
             foreach ($dom->getElementsByTagName('menu') as $position => $btn) {
                 ?>
-                <input type="submit" name="<?= $position; ?>" value="<?= $btn->nodeValue; ?>"/>
+            <a href="/<?= $position + 1;?>.html"><?= $btn -> nodeValue; ?></a>
                 <?php
             }
             ?>
         </form>
         <?php
+        // affiche la page desirez 
         $ciblePage1 = $dom->getElementsByTagName('content')->item($page);
         echo $ciblePage1->nodeValue;
         ?>
