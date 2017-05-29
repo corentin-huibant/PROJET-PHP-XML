db74d89bbc23472bd620918623499f57df6992e4
PriscilliaL <lefebvre.priscillia60@gmail.com>
2017-05-29 11:09:03 +0200

    Ajout du css

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $dom->load('source.xml');
 $page = 0;
 // Si on est sur une page on affiche le contenu attitré à cette page.
 if (isset($_GET['page'])) { // && $_GET['page'] < getElementsByTagName('menu')
-    $page = $_GET['page'] -1;
+    $page = $_GET['page'] - 1;
 }
 ?>
 <html>
@@ -15,22 +15,51 @@ if (isset($_GET['page'])) { // && $_GET['page'] < getElementsByTagName('menu')
         <meta charset="utf-8">
         <!-- On affiche le titre selon la page où l'on ce trouve. --> 
         <title><?= $dom->getElementsByTagName('title')->item($page)->nodeValue; ?></title>
-        <link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Baloo|Clicker+Script|Lobster|News+Cycle" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <?php
-        // Dans le fichier XML on récupère le noeud <menu> avec sa position et on le met dans le menu
-        foreach ($dom->getElementsByTagName('menu') as $position => $nav) {
-            ?>
-            <!-- L'input type submit récupère la position où l'on ce trouve et sa valeur. -->
-            <a href=" /<?= $position +1; ?>.html"><?= $nav -> nodeValue; ?></a>
-            <?php
-        }
-        ?>
-        <?php
-        // On affiche le contenu des pages.
-        $ciblePage = $dom->getElementsByTagName('content')->item($page);
-        echo $ciblePage->nodeValue;
-        ?>
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <?php
+                        /* On utilise la boucle foreach pour ressortir le contenu des différentes balise 'menu' dans des balises 'anchor'
+                          à chaque itération, la valeur de l'élément est assignée à la variable $nav. */
+                        foreach ($dom->getElementsByTagName('menu') as $position => $nav) {
+                            ?>
+                            <!-- Dans l'attribut href, on affiche l'index assigné lors de l'ittération à nos variables $nav en ajoutant 1 pour simuler la récupération de l'id, suivi de l'extension .html-->
+                            <li><a href="<?= $position + 1 ?>.html"><?= $nav->nodeValue; ?></a></li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <?php
+                    // On affiche le contenu des pages.
+                    $ciblePage = $dom->getElementsByTagName('content')->item($page);
+                    echo $ciblePage->nodeValue;
+                    ?>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
