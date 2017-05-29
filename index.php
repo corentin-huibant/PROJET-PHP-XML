ae1645aabf054297730048306b40a747fcbbcbd1
Corentin <corentin.huibant@laposte.net>
2017-05-29 11:28:24 +0200

    le produit terminé

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $dom = new DOMDocument();
 //On fait ensuite appel à la méthode load() pour charger le fichier voulu
 $dom->load('source.xml');
+// Initialisation de la variable $page qui représente le numéro de la page.
 $page = 0;
 //la variable $count calcul le nombre de noeud page dans le document html
 $count = $dom->getElementsByTagName('page')->length;
@@ -13,31 +14,58 @@ if(isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] <= $count 
    $page = $_GET['page'] - 1;
 }
 ?>
-<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <!-- On récupère les informations du fichier XML dans notre DomDocument en faisant appel à la méthode getElementsByTagName
-        Puis, s'il y a plusieur balises du même nom, on utilise la méthode item qui nous permet de cibler les indices 
-        de chaque balise du même nom comme dans un tableau -->
+        <!-- On affiche le titre selon la page où l'on ce trouve. --> 
         <title><?= $dom->getElementsByTagName('title')->item($page)->nodeValue; ?></title>
-        <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Baloo|Clicker+Script|Lobster|News+Cycle" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-            <?php
-            /*On utilise la boucle foreach pour ressortir le contenu des différentes balise 'menu' dans des balises 'anchor'
-            à chaque itération, la valeur de l'élément est assignée à la variable $nav. */
-            foreach($dom->getElementsByTagName('menu') as $position => $nav) {
-            ?>
-                <!-- Dans l'attribut href, on affiche l'index assigné lors de l'ittération à nos variables $nav en ajoutant 1 pour simuler la récupération de l'id, suivi de l'extension .html-->
-                <a href="<?= $position + 1 ?>.html"><?= $nav->nodeValue; ?></a>
-            <?php
-            }
-            //Pour finir, on récupère les balises 'content' du fichier XML en fonction de la valeur de la variable $page...
-            $ciblePage = $dom->getElementsByTagName('content')->item($page);
-            //Puis on renvoie le contenu du noeud (content) en faisant appel à la propriété nodeValue
-            echo $ciblePage->nodeValue;
-            ?>
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <!-- On récupère les informations du fichier XML dans notre DomDocument en faisant appel à la méthode getElementsByTagName
+                        Puis, s'il y a plusieur balises du même nom, on utilise la méthode item qui nous permet de cibler les indices 
+                        de chaque balise du même nom comme dans un tableau -->
+                        <?php
+                        foreach ($dom->getElementsByTagName('menu') as $position => $nav) {
+                            ?>
+                            <!-- Dans l'attribut href, on affiche l'index assigné lors de l'ittération à nos variables $nav en ajoutant 1 pour simuler la récupération de l'id, suivi de l'extension .html-->
+                            <li><a href="<?= $position + 1 ?>.html"><?= $nav->nodeValue; ?></a></li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <?php
+                    //Pour finir, on récupère les balises 'content' du fichier XML en fonction de la valeur de la variable $page...
+                    $ciblePage = $dom->getElementsByTagName('content')->item($page);
+                    //Puis on renvoie le contenu du noeud (content) en faisant appel à la propriété nodeValue
+                    echo $ciblePage->nodeValue;
+                    ?>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
