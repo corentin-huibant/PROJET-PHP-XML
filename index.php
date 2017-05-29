674749e321d2b6a9933698c56df171cb91130a32
PriscilliaL <lefebvre.priscillia60@gmail.com>
2017-05-29 11:42:29 +0200

    Dernière modifications

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
-// Instancie la variable $dom en DOMDocument
+//on instancie un objet de la classe DOMDocuent qui représente un fichier XML (ou HTML) entier. Il s'agit donc de la racine de l'arbre document.
 $dom = new DOMDocument();
-// Charge le fichier XML
+//On fait ensuite appel à la méthode load() pour charger le fichier voulu
 $dom->load('source.xml');
 // Initialisation de la variable $page qui représente le numéro de la page.
 $page = 0;
-// Si on est sur une page on affiche le contenu attitré à cette page.
-if (isset($_GET['page'])) { // && $_GET['page'] < getElementsByTagName('menu')
-    $page = $_GET['page'] - 1;
+//la variable $count calcul le nombre de noeud page dans le document html
+$count = $dom->getElementsByTagName('page')->length;
+//la variable $_GET renvoie un tableau associatif dans lequel on recherche la valeur de 'page'. Si elle est présente...
+//on vérifie aussi qu'il s'agit bien d'un chiffre et que ce chiffre n'est pas supérieur au nombre de noeuds 'page'
+if(isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] <= $count && $_GET['page'] > 0) {
+    //La variable $page est égale à la valeur de 'page' - 1
+   $page = $_GET['page'] - 1;
 }
 ?>
 <html>
@@ -21,7 +25,7 @@ if (isset($_GET['page'])) { // && $_GET['page'] < getElementsByTagName('menu')
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link href="assets/css/style.css" rel="stylesheet" type="text/css">
     </head>
-    <body>
+      <body>
         <nav class="navbar navbar-default navbar-static-top" role="navigation">
             <div class="container">
                 <div class="navbar-header">
@@ -36,9 +40,10 @@ if (isset($_GET['page'])) { // && $_GET['page'] < getElementsByTagName('menu')
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
+                        <!-- On récupère les informations du fichier XML dans notre DomDocument en faisant appel à la méthode getElementsByTagName
+                        Puis, s'il y a plusieur balises du même nom, on utilise la méthode item qui nous permet de cibler les indices 
+                        de chaque balise du même nom comme dans un tableau -->
                         <?php
-                        /* On utilise la boucle foreach pour ressortir le contenu des différentes balise 'menu' dans des balises 'anchor'
-                          à chaque itération, la valeur de l'élément est assignée à la variable $nav. */
                         foreach ($dom->getElementsByTagName('menu') as $position => $nav) {
                             ?>
                             <!-- Dans l'attribut href, on affiche l'index assigné lors de l'ittération à nos variables $nav en ajoutant 1 pour simuler la récupération de l'id, suivi de l'extension .html-->
@@ -54,8 +59,9 @@ if (isset($_GET['page'])) { // && $_GET['page'] < getElementsByTagName('menu')
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <?php
-                    // On affiche le contenu des pages.
+                    //Pour finir, on récupère les balises 'content' du fichier XML en fonction de la valeur de la variable $page...
                     $ciblePage = $dom->getElementsByTagName('content')->item($page);
+                    //Puis on renvoie le contenu du noeud (content) en faisant appel à la propriété nodeValue
                     echo $ciblePage->nodeValue;
                     ?>
                 </div>
